57083d3150147f315b22ea35b60539d790747bb3
5arthak01 <sarthak.agrawal@research.iiit.ac.in>
2021-02-20 21:50:03 +0530

    Added MT Times articles

--- tutorial/spiders/scrape_url.py
+++ tutorial/spiders/scrape_url.py
@@ -7,14 +7,16 @@ class UrlSpider(scrapy.Spider):
     def start_requests(self):
         urls = [
             f"https://maharashtratimes.com/topics/%E0%A4%B6%E0%A5%87%E0%A4%A4%E0%A4%95%E0%A4%B0%E0%A5%80-%E0%A4%86%E0%A4%82%E0%A4%A6%E0%A5%8B%E0%A4%B2%E0%A4%A8/{x}"
-            for x in range(1, 3)
+            for x in range(2, 22)
         ]
 
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        urls = response.css("div.newslistbx h2 a::attr(href)").getall()
+        urls = response.css(
+            "li.news-card.horizontal-lead.col6.news span.con_wrap a::attr(href)"
+        ).getall()
         filename = "urls.txt"
         with open(filename, "a") as f:
             for item in urls:
