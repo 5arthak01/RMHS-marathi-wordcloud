233586bc1992a79fe71eb726c1d908e5de823a0e
RayChithra <59470048+RayChithra@users.noreply.github.com>
2021-02-21 21:40:46 +0530

    Delete filter_urls.py

--- filter_urls.py
+++
@@ -1,9 +0,0 @@
-urls = []
-with open("urls.txt", "r") as f:
-    urls = f.readlines()
-
-filtered_urls = set(urls)
-
-with open("filtered_urls.txt", "a") as f:
-    for url in filtered_urls:
-        f.write(url)
